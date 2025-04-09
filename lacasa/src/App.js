f1fb62b7a1dda05bb0ab2cadba87ae5eaccf034a
Arb Sahatqija <as67822@ubt-uni.net>
2025-04-09 18:29:01 +0200

    Added Fadein When Opening the site for the first time

--- lacasa/src/App.js
+++ lacasa/src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Navbar from './components/Navbar';
 import HeroSection from './components/HeroSection';
 import About from './components/About';
@@ -7,27 +7,30 @@ import Gallery from './components/Gallery';
 import Footer from './components/Footer';
 import Services from './components/Services';
 import Contact from './components/Contact';
-import './styles/main.css';
 import ScrollToTop from './components/ScrollToTop';
-
+import './styles/main.css';
+import './components/fadein.css'; // Correct path to your new fade-in style
 
 function App() {
+  const [isVisible, setIsVisible] = useState(false);
+
+  useEffect(() => {
+    // Trigger the fade-in effect on mount
+    setIsVisible(true);
+  }, []);
+
   return (
-    <>
+    <div className={`fade-in-wrapper ${isVisible ? 'visible' : ''}`}>
       <Navbar />
       <HeroSection />
       <About />
       <Slider />
       <Gallery />
-      <Services/>
-      <Contact/>
-      <ScrollToTop /> 
-
-      {/* Footer Stays in the end of the page */}
-      
+      <Services />
+      <Contact />
+      <ScrollToTop />
       <Footer />
-      
-    </>
+    </div>
   );
 }
 
